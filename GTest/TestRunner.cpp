8d1d7fd0c7f45400be7cec7c99a7903b28ca1c13
RobertoVelazquezGit <vmroberto@hotmail.es>
2026-02-11 22:45:52 +0100

    Flags to select test

--- GTest/TestRunner.cpp
+++ GTest/TestRunner.cpp
@@ -106,4 +106,75 @@ TEST(SquareRootTest, PositiveArgumentTest)
     ASSERT_NO_THROW(mySqrt(inputValue));
 }
 
+// #define __WIN32
+// or WIN32, or __WIN32__
+
+TEST(UnixSuite, DummyTest1)
+{
+    std::cout << "Dummy test 1 - Unix \n";
+#ifdef /*__WIN32*/ _WIN64
+    FAIL();
+#else
+    SUCCEED();
+#endif
+}
+
+
+TEST(UnixSuite, DummyTest2)
+{
+    std::cout << "Dummy test 2 - Unix \n";
+#ifdef _WIN64
+    FAIL();
+#else
+    SUCCEED();
+#endif
+}
+
+
+TEST(WindowsSuite, DummyTest1)
+{
+    std::cout << "Dummy test 1 - Windows \n";
+#ifdef _WIN64
+    SUCCEED();
+#else
+    FAIL();
+#endif
+}
+
+
+TEST(WindowsSuite, DummyTest2)
+{
+    std::cout << "Dummy test 2 - Windows \n";
+#ifdef _WIN64
+    SUCCEED();
+#else
+    FAIL();
+#endif
+}
+
+TEST(WindowsSuite, DummyTest3)
+{
+    std::cout << "Dummy test 3 - Windows \n";
+#ifdef _WIN64
+    SUCCEED();
+	// No in this version of googletest Microsoft GTEST_SKIP() << "Skipping this test for demonstration purposes.";   
+#else
+    FAIL();
+#endif
+}
+
+
+
+//int main(int argc, char** argv)
+//{
+//#ifdef _WIN64
+//    testing::GTEST_FLAG(filter) = "WindowsSuite.*";
+//#else
+//    testing::GTEST_FLAG(filter) = "UnixSuite.*";
+//#endif
+//    testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
+
+
 
